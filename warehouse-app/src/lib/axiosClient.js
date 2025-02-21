d41b63ff672aa676b6211c789398f7c185452bb7
chris01b <cbid@sas.upenn.edu>
2025-02-20 19:02:49 -0500

    Add simple polling mechanism to clients' apis

--- warehouse-app/src/lib/axiosClient.js
+++ warehouse-app/src/lib/axiosClient.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 export default axios.create({
   baseURL: 'http://localhost:5000/api/warehouse',
@@ -6,3 +7,18 @@ export default axios.create({
     'Content-Type': 'application/json'
   }
 })
+
+// Simple polling mechanism
+const useRequestRefresh = (interval = 10000) => {
+  const [refreshCount, setRefreshCount] = useState(0)
+  
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRefreshCount(prev => prev + 1)
+    }, interval)
+    
+    return () => clearInterval(timer)
+  }, [interval])
+
+  return refreshCount
+}
